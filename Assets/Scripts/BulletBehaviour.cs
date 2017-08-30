d3762cb8a70edd0a352104bcb8c8520d4a616f3c
Dan Ward <dan.steve@hotmail.co.uk>
2017-08-30 17:50:20 +0100

    bullet beheviour
    handles givivng damage

---
+++ Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour {
+
+    public int damage;
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(gameObject.name + " hit: " + other.collider.name );
+        other.transform.SendMessage(("TakeDamage"), damage, SendMessageOptions.DontRequireReceiver);
+        Destroy(gameObject);
+    }
+
+}
+
