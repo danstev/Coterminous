8dd0048eab7ec71c04aa7c95cba25f5855058b76
Dan Ward <dan.steve@hotmail.co.uk>
2017-09-07 16:51:44 +0100

    New main menu handling script

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void CloseGame()
+    {
+        Debug.Log("Exiting Game");
+        Application.Quit();
+    }
+
+    public void NewGame()
+    {
+        //SceneManager.LoadScene("Scene");
+        Debug.Log("New Game");
+    }
+
+    public void SaveGame()
+    {
+        Debug.Log("Save Game");
+    }
+
+    public void LoadGame()
+    {
+        Debug.Log("Load Game");
+    }
+
+    public void Options()
+    {
+        Debug.Log("Options");
+    }
+
+    public void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+}
